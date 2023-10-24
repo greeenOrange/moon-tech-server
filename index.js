515369d2227a12c60e89b6aed09a53ae4bb6e6a0
greeenOrange <gm1sadiq@gmail.com>
2023-10-24 23:40:31 +0600

    klsdjfsa

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 5000;
 server.use(middlewares);
 server.use(router);
 
-app.listen(port, () =>{
+server.listen(port, () =>{
     console.log('Running the server on Port', port);
 })
