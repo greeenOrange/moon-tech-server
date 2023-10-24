a3e0d64da1881a118a09b55616cc468e7b25600b
greeenOrange <gm1sadiq@gmail.com>
2023-10-24 23:20:41 +0600

    check

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 5000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+app.listen(port, () =>{
+    console.log('Running the server on Port', port);
+})
